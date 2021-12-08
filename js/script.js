0679038f6f1ae58b9ce780a9575a94cdc3b12c3b
sizhongbin <56616211+sizhongbin@users.noreply.github.com>
2021-12-08 10:52:53 +0800

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,71 @@
+function initYou() {
+  var you = {
+    currentStage: "0",
+    jobClearedStage: [],
+    sensedEnemy: ["波利",
+      "绿棉虫",
+      "疯兔"],
+    jobLv: {
+      "基本": 3,
+      "初心者": 3
+    },
+    currentJob: "初心者",
+    attribute: "无",
+    race: "人形",
+    size: "中",
+    zeny: 0,
+    stats: {
+      str: 2,
+      agi: 1,
+      vit: 2,
+      int: 1,
+      dex: 1,
+      luk: 1
+    },
+    equip: {
+      "头上": {
+        name: "空",
+        card: []},
+      "头中": {
+        name: "空",
+        card: []},
+      "头下": {
+        name: "空",
+        card: []},
+      "身体": {
+        name: "冒险衣",
+        card: []},
+      "主手": {
+        name: "短剑",
+        card: []},
+      "副手": {
+        name: "空",
+        card: []},
+      "披挂": {
+        name: "空",
+        card: []},
+      "鞋子": {
+        name: "空",
+        card: []},
+      "饰品一": {
+        name: "空",
+        card: []},
+      "饰品二": {
+        name: "空",
+        card: []}},
+    learnedSkill: {
+      "初心者": {
+        "基本技能": 1,
+        "紧急治疗": 1
+      }
+    },
+    carriedItem: {
+      "初学者专用药水": 1
+    },
+    assist: [],
+    storeItem: [],
+    guild: "无"
+  };return you;
+}
 
+function main(){console.log(initYou());return 1;}

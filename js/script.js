2f691080fb9d95a9d18324bf89866b1afe790cd0
sizhongbin <56616211+sizhongbin@users.noreply.github.com>
2021-12-10 16:41:35 +0800

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,93 @@
-/* 角色初始化 */
-function initYou() {
-  var you = {
+/* 角色数据 */
+var you;
+/* 存档初始化，为避免cookie超过4K尽量减少字符数 */
+function initSave() {
+  var save = [
+    /* 0:关卡进度，存已通关关卡键。下标0为总进度，1-为各职业进度，对应职业键 */
+    [0],
+    /* 1:怪物情报，存已有情报怪物键*/
+    [],
+    /* 2:职业等级，存等级。下标0为基本等级，1-为各职业等级，对应职业键 */
+    [3,
+      3],
+    /* 3:当前职业键 */
+    1,
+    /* 4:初始属性键 */
+    0,
+    /* 5:初始种族键 */
+    0,
+    /* 6:初始体型键 */
+    1,
+    /* 7:钱 */
+    0,
+    /* 8:六维，下标对应savidl */
+    [2,
+      1,
+      2,
+      1,
+      1,
+      1],
+    /* 9:装备，[部位[装备,[卡片]]]，最后为投射物，存键 */
+    [[0,
+      []],
+      [0,
+        []],
+      [0,
+        []],
+      [2,
+        []],
+      [0,
+        []],
+      [1,
+        []],
+      [0,
+        []],
+      [0,
+        []],
+      [0,
+        []],
+      [0,
+        []],
+      0],
+    /* 10:习得技能，[职业,[技能]]，存键 */
+    [[1,
+      [0,
+        1]]],
+    /* 11:携带物品，[大类,道具,数量]，存键 */
+    [[2,
+      0,
+      1]],
+    /* 12:支援，存键 */
+    [],
+    /* 13:仓库物品，[大类,道具,数量]，存键 */
+    [],
+    /* 14:公会名，0无，1天禁仙境 */
+    0
+  ];
+  Cookies.set("s", JSON.stringify(save), {
+    expires: 365
+  });
+  return;
+}
+/* 通过值找键*/
+function findKey(obj, value, compare = (a, b) => a === b) {
+  return Object.keys(obj).find(k => compare(obj[k], value))
+}
+/* 职业名键值互转，用in检索，by为key则输出value，by为value则输出key*/
+function getJobKeyOrValue(input, by = "key") {
+  var job = {
+    0: "基本",
+    1: "初心者",
+    2: "剑士"
+  };
+  if (by === "key")return job[input];
+  else if (by === "value")return findKey(job, input);
+  else return 0;
+}
+/* 将存档转换为角色数据*/
+function convertSaveToYou() {
+  var save = JSON.parse(Cookies.get("s"));
+  you = {
     currentStage: "0",
     jobClearedStage: [],
     sensedEnemy: ["波利",
@@ -54,23 +141,24 @@ function initYou() {
       "饰品二": {
         name: "空",
         card: []}},
-    learnedSkill: {
-      "初心者": {
-        "基本技能": 1,
-        "紧急治疗": 1
-      }
-    },
-    carriedItem: {
-      "初学者专用药水": 1
-    },
+    learnedSkill: [{
+      job: "初心者",
+      skill: [{
+        name: "基本技能",
+        lv: 1
+      },
+        {
+          name: "紧急治疗",
+          lv: 1
+        }]}],
+    carriedItem: [{
+      name: "初学者专用药水",
+      quantity: 1
+    }],
     assist: [],
     storeItem: [],
     guild: "无"
   };
-  Cookies.set("saveData", JSON.stringify(you), {
-    expires: 365
-  });
-  return you;
 }
 /* 关卡数据*/
 function stageData(stageNumber) {
@@ -86,7 +174,7 @@ function stageData(stageNumber) {
 /* 写入关卡敌人 */
 function displayStageEnemy(selectedStage) {
   var container = "";
-  var you = JSON.parse(Cookies.get("saveData"));
+  var you = JSON.parse(Cookies.get("s"));
   for (let i = 0; i < stageData(selectedStage).enemy.length; i++) {
     container += "<tr><td>"+(i+1)+"</td><td>"+stageData(selectedStage).enemy[i]+"</td><td>"; if (you.sensedEnemy.includes(stageData(selectedStage).enemy[i]))container += "<button>详细</button></td></tr>"; else container += "不明</td></tr>"
   }
@@ -134,8 +222,8 @@ function stageInfo(preId, selectedStage) {
 function gameLoad(isNewGame) {
   /* 取关卡进度 */
   var currentStage;
-  if (isNewGame)currentStage = initYou().currentStage;
-  else currentStage = JSON.parse(Cookies.get("saveData")).currentStage;
+  if (isNewGame)currentStage = initSave().currentStage;
+  else currentStage = JSON.parse(Cookies.get("s")).currentStage;
   if (currentStage !== "0") {
     /* 进选关界面*/
   } else {
@@ -149,7 +237,7 @@ function gameStart() {
   /* 显示版本号 */
   document.getElementById("ver").innerHTML = "Dev.20211210";
   /* 判断存档是否存在 */
-  var saveData = Cookies.get("saveData");
+  var saveData = Cookies.get("s");
   if (!saveData) {
     let button = document.createElement("button");
     button.innerHTML = "开始游戏";
